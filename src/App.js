5a7bf9acd691b52b01b04b17d4373b8d6708e085
Guillem <guillem.dardill@gmail.com>
2021-02-03 11:31:14 +0100

    sign-in / sign-up pages added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
-// import SignIn from './components/SignIn/SignIn';
-// import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 // import Annonces from './components/Annonces/Annonces';
 // import FicheDetail from './components/Annonces/FicheDetail';
 
@@ -17,13 +17,13 @@ function App() {
           <Route exact path='/'>
             <Home />
           </Route>
-          {/* <Route path='/signIn'>
+          <Route path='/signIn'>
             <SignIn />
           </Route>
           <Route path='/signUp'>
             <SignUp />
           </Route>
-          <Route exact path='/annonces'>
+          {/* <Route exact path='/annonces'>
             <Annonces />
           </Route>
           <Route path='/annonces/:idAnimal'>
