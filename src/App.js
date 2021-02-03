5dfcce846d3383b87533f4bbea9e7e045cc1406b
Guillem <guillem.dardill@gmail.com>
2021-02-03 09:40:52 +0100

    project architecture creation, npm packages installation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className='App' style={{ height: '100%', width: '100%' }}></div>
+    </Router>
+  );
+}
+
+export default App;
